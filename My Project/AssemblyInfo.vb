f66b3b8f3b0424cdee5f5a6f08d712b7cb45c93c
rackd <engrkell202@gmail.com>
2022-07-19 15:29:04 -0400

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("KeyboardCOM")>
+<Assembly: AssemblyDescription("KeyboardCOM - Hook/Hotkey")>
+<Assembly: AssemblyCompany("RACKD")>
+<Assembly: AssemblyProduct("KeyboardCOM")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1484c61b-f2ec-49d3-894c-c9f5aa7693ad")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0")>
+<Assembly: AssemblyFileVersion("1.0")>
